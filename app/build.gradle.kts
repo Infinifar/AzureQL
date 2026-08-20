40ad9c310c18187b6c54c25cd3d1467f38ad7d7f
yisilan83 <yisilan83@users.noreply.github.com>
2026-08-20 12:02:20 +0000

    chore: bump version to 2.1.0 (code 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.autopanel.app"
         minSdk = 31
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.0.0"
+        versionCode = 8
+        versionName = "2.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
